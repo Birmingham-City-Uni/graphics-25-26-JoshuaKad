e38753e95b57a57a8ffeb25504e7aa78d87ffb02
Joshua <114919337+JoshuaKad@users.noreply.github.com>
2026-05-06 11:01:03 +0100

    Update Shading.hpp

--- Labs/week8/Shading.hpp
+++ Labs/week8/Shading.hpp
@@ -12,9 +12,9 @@ Eigen::Vector3f reflect(const Eigen::Vector3f& incoming, const Eigen::Vector3f& 
 {
 	// *** YOUR CODE HERE ***
 	// replace this with the reflected vector.
-	return Eigen::Vector3f::Zero();
+	return incoming - 2.0f * incoming.dot(normal) * normal;
 	// *** END YOUR CODE ***
-}
+}	
 
 // Subtask 2: Implement the Phong specular term in Shading.hpp, to find the intensity of specular reflection
 /// <summary>
@@ -30,16 +30,15 @@ float phongSpecularTerm(const Eigen::Vector3f& incomingLightDir, const Eigen::Ve
 {
 	// *** YOUR CODE HERE ***
 	// Find the reflected direction using the reflect function
-	Eigen::Vector3f reflectionDir = Eigen::Vector3f::Zero();
+	Eigen::Vector3f reflectionDir = reflect(-incomingLightDir, normal);
 
 	// Find dot product between reflected and view directions.
-	float reflectDotNorm = 0.f;
+	float reflectDotNorm = reflectionDir.dot(viewDir);
 
 	// Make sure dot product is non-negative (if it's less than 0, set it to 0!)
-	reflectDotNorm = 0.f;
-
+	reflectDotNorm = std::max(reflectDotNorm, 0.0f);
 	// Finally, raise to specular exponent and return.
-	return 0.f;
+	return std::pow(reflectDotNorm, exponent);
 	// *** END YOUR CODE ***
 }
 
